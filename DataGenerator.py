4afeb084877e3517d590407e400941b0b93c1652
Juuso Rytilahti <rytilahti.juuso@gmail.com>
2021-07-08 17:08:06 +0300

    Renaming and refactoring a bit

--- DataGenerator.py
+++ DataGenerator.py
@@ -12,14 +12,21 @@ class DataGenerator(object):
         self.female_first_names  = pd.read_excel (r'C:\Users\rytil\Documents\Github\seating-order-organizer\etunimitilasto-2021-02-05-dvv.xlsx', sheet_name='Naiset kaikki')["Etunimi"]
         self.surnames = pd.read_excel (r'C:\Users\rytil\Documents\Github\seating-order-organizer\sukunimitilasto-2021-02-05-dvv.xlsx', sheet_name='Nimet')["Sukunimi"]
         self.generated_names = []
-        self.generate_simple_order()
-        
-    def generate_simple_order(self):
-        for i in range(0, 100):
+        self.generate_unique_names()
+    
+    # Generate unique names for participants
+    def generate_unique_names(self):
+        surname_index = 0 # Same surnames for every group that has asked each other as seating company. This is simply for convenience and easier checkming later the results of seating organizer algorithm by hand
+        for i in range(0, 100):   
+            if(i % 5 == 0 and i != 0):
+                surname_index = i
             if(i % 2 == 0):
-                self.generated_names.append(self.male_first_names[i])
+                self.generated_names.append(self.male_first_names[i] + " " + self.surnames[surname_index])
             else:
-                self.generated_names.append(self.female_first_names[i])
+                self.generated_names.append(self.female_first_names[i] + " " + self.surnames[surname_index])
+    
+    def generate_name_and_friend_list(self):
+        
 
 if __name__:
     print("Data generator is run in namespace")
