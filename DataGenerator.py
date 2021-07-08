7a286617295014dffd07952ae50a882c48283dc8
Juuso Rytilahti <rytilahti.juuso@gmail.com>
2021-07-08 16:39:05 +0300

    simple order of first names is generated correctly

--- DataGenerator.py
+++ DataGenerator.py
@@ -10,9 +10,20 @@ class DataGenerator(object):
     def __init__(self):
         self.male_first_names = pd.read_excel (r'C:\Users\rytil\Documents\Github\seating-order-organizer\etunimitilasto-2021-02-05-dvv.xlsx', sheet_name='Miehet kaikki')["Etunimi"]
         self.female_first_names  = pd.read_excel (r'C:\Users\rytil\Documents\Github\seating-order-organizer\etunimitilasto-2021-02-05-dvv.xlsx', sheet_name='Naiset kaikki')["Etunimi"]
-        self.surnames = "asd"
+        self.surnames = pd.read_excel (r'C:\Users\rytil\Documents\Github\seating-order-organizer\sukunimitilasto-2021-02-05-dvv.xlsx', sheet_name='Nimet')["Sukunimi"]
+        self.generated_names = []
+        self.generate_simple_order()
+        
+    def generate_simple_order(self):
+        for i in range(0, 100):
+            if(i % 2 == 0):
+                self.generated_names.append(self.male_first_names[i])
+            else:
+                self.generated_names.append(self.female_first_names[i])
 
 if __name__:
     print("Data generator is run in namespace")
     d = DataGenerator()
-    print(d.male_first_names)
+    print(d.generated_names)
+    #print(d.female_first_names)
+    #print(d.surnames)
