a8fe674b03e78d529e8eb0d442e93a5c3d4801ed
Juuso Rytilahti <rytilahti.juuso@gmail.com>
2021-07-09 18:13:32 +0300

    Score by gender is working correctly, read desc!
    - Changed from "miehet kaikki" to "miehet ens" in data generation because men and women share some duplicate front names and I don't wan't that when model is trained because then calculation optimal score wouldn't be so easy.

--- DataGenerator.py
+++ DataGenerator.py
@@ -5,10 +5,11 @@ Created on Thu Jul  8 16:00:54 2021
 @author: rytil
 """
 import pandas as pd
+import random
 
 class DataGenerator(object):
     def __init__(self):
-        self.male_first_names = pd.read_excel (r'C:\Users\rytil\Documents\Github\seating-order-organizer\etunimitilasto-2021-02-05-dvv.xlsx', sheet_name='Miehet kaikki')["Etunimi"]
+        self.male_first_names = pd.read_excel (r'C:\Users\rytil\Documents\Github\seating-order-organizer\etunimitilasto-2021-02-05-dvv.xlsx', sheet_name='Miehet ens')["Etunimi"]
         self.female_first_names  = pd.read_excel (r'C:\Users\rytil\Documents\Github\seating-order-organizer\etunimitilasto-2021-02-05-dvv.xlsx', sheet_name='Naiset kaikki')["Etunimi"]
         self.surnames = pd.read_excel (r'C:\Users\rytil\Documents\Github\seating-order-organizer\sukunimitilasto-2021-02-05-dvv.xlsx', sheet_name='Nimet')["Sukunimi"]
         self.generated_names = []
@@ -47,26 +48,50 @@ class DataGenerator(object):
 class ScoreCalculation(object):
     def __init__(self, name_and_friend_list, male_first_names):
         self.name_and_friend_list = name_and_friend_list
-        self.male_first_names = male_first_names
+        self.male_first_names = []
+        #FIXME make in operator work some other way
+        for i in range(0, len(male_first_names)): # problem with 'in' operator if this is not done this way
+            self.male_first_names.append(male_first_names[i])
         self.baseline_score = 0 # Generated ny shuffling the perfect order list
         self.best_score = 0
-        
+        self.participant_list = []
         self.best_order = []
+        
         
         # Temporary solution, wil be deleted when score calcucalation is done
         for i in range(0, len(name_and_friend_list)):
             name = name_and_friend_list[i][0]
             self.best_order.append(name)
-            
+            self.participant_list.append(name) 
+        #random.shuffle(self.participant_list) # TODO remove
+        
+        
         self.generate_empty_score_table_2d()
-            
+        self.add_scores_to_table()
+        
+    # Generate empty score table with starting base score of 10        
     def generate_empty_score_table_2d(self):
         self.score_table_2d= []
-        for i in range(0, len(self.best_order)):
+        for i in range(0, len(self.participant_list)):
             tmp= []
-            for j in range(0, len(self.best_order) ):
-                tmp.append(0)
+            for j in range(0, len(self.participant_list) ):
+                tmp.append(10)
             self.score_table_2d.append(tmp)
+            
+    def add_scores_to_table(self):
+        for i in range(0, len(self.participant_list)):
+            front_name = self.participant_list[i].split(sep= " ", maxsplit=2)[0]
+            self.add_score_based_on_gender(i, front_name in self.male_first_names)
+            
+    # checked_index: person who is checked
+    # is_man: previusly checked that person's first name is found from list of men names
+    def add_score_based_on_gender(self, checked_index, is_man):
+        same_gender_added_score = 2
+        for i in range(0, len(self.participant_list)):
+            #Take the front name 
+            front_name = self.participant_list[i].split(sep= " ", maxsplit=2)[0]
+            if (front_name in self.male_first_names and  is_man == False)or (front_name not in self.male_first_names and is_man == True) : #TODO this currently only supports men, not women
+                self.score_table_2d[checked_index][i] = self.score_table_2d[checked_index][i] + same_gender_added_score 
     
     def calculate_score(self):
         print("asd")
@@ -86,7 +111,7 @@ class ScoreCalculation(object):
 if __name__:
     print("Data generator is run in namespace")
     d = DataGenerator()
-    print(d.generated_order[10][0])
+    s = ScoreCalculation(d.name_and_friend_list, d.male_first_names)
     print(d.generated_order[10][1])
     #print(d.female_first_names)
     #print(d.surnames)
